5b1d3198726bc5bab4eac5698dc40cd0d13793fb
Espace NUMERIQUE 8 <espace8@numerique.lan>
2024-10-10 12:50:48 +0300

    ajout de fichier JS

---
+++ main.js
@@ -0,0 +1,9 @@
+let nom = "Faiz"  //declarer une variable 
+
+let village = "Hagnoundrou"
+
+let bienvenu = "Bienvenu dans le village de Hagnoundrou"
+
+let messageaccueil = nom + " " + village + " " + bienvenu;
+
+alert(messageaccueil) // fonction intégrante de JS. permet de mttre un alerte au niveau du page
