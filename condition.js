cc31cd5621f4333aaf085937a552633f6d7fa9aa
Faiz-git99 <marifaiz97@gmail.com>
2024-10-12 11:54:37 +0300

    condition

---
+++ condition.js
@@ -0,0 +1,22 @@
+// les conditions
+/*Si A existe, alors B s'affiche*/ 
+
+//On utilise le clé if() pour créer une condition
+
+
+
+/*
+ Si j'ai la note = 5 alors affichage insuffiant
+ Si note = 10 affichage passable
+ Si note = 12 affichage assez-bien
+ Si note = 16 affichage bien
+ Si note = 18 affichage très bien
+ Si note = 20 affichage très très bien 
+*/
+
+let note = 10;
+
+if (note == 10){
+   alert("Passable");
+}
+ 
