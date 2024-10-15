032eea77db9a39802df61759254926408ce7544a
Faiz-git99 <marifaiz97@gmail.com>
2024-10-15 15:55:53 +0300

    inserer un tableau sur JS

---
+++ tableau.js
@@ -0,0 +1,18 @@
+/* Les Tableaux */
+
+let tableau = ["Faiz", "mayotte", 12, false];
+
+//console.log permet d'afficher le message
+console.log ("j'ai trouvé ", tableau[1]);
+
+// je declare mon objet 'client'
+let client = {
+    nom:"Ali",
+    prenom:"Mze",
+    numeroclient: 125,
+    adresse: "2 Boulevard Madi -mayotte",
+    celibatire:  false
+};
+
+//console.log permet d'afficher le message
+console.log("Mon client s'appelle", client.nom, client.prenom, " et il habite à", client.adresse, "son nnuméro de client" , client.numeroclient);
