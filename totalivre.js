02d9d56c0fc0d2bd59d82a700ce46266b0f21d83
Faiz-git99 <marifaiz97@gmail.com>
2024-10-15 15:57:51 +0300

    additionner et soustrait une variable

---
+++ totalivre.js
@@ -0,0 +1,11 @@
+let totalLivres = 500; // affichage de 500 livres
+    totalLivres += 50; // ajout de 50 livres de plus
+    totalLivres -= 10; // perdre 10 livres
+    totalLivres += 5; // rajoute 5 livres jetés
+
+console.log(totalLivres)
+
+let affichageTotalLivres = "le nombre affiché est de "
+
+console.log(affichageTotalLivres + totalLivres) /*le message est affichéé + la valeur du totatlivre*/ 
+
