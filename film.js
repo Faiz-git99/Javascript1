08bd2b5a7c49c8f0e94f0f9d882842d3dbe66ff3
Faiz-git99 <marifaiz97@gmail.com>
2024-10-16 12:01:59 +0300

    ticket cinema

---
+++ film.js
@@ -0,0 +1,20 @@
+// Récuperation de billet de film Koungou
+
+let ticketfilm = {
+    nomfilm : "Koungou",
+    heurefilm : "17h00",
+    placefilm : "12A",
+    prixfilm : 7,
+};
+let Affichagefilm = "Voilà votre billet pour le film ";
+console.log(Affichagefilm + ticketfilm.nomfilm)
+
+let ticket = {
+    nomFilm : "Wakanda forever",
+    prix : 5,
+    numeroSalle : 3,
+};
+let nom = "Moussa";
+
+console.log(" Bonjour "+ nom + "," +"votre film " +ticket.nomFilm +" est en salle " + ticket.numeroSalle)
+
