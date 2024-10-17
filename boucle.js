548c659730df6cdf9a3d5808f2fd6ba57fe1c413
Faiz-git99 <marifaiz97@gmail.com>
2024-10-17 06:54:39 +0300

    inserer la condition while et for

---
+++ boucle.js
@@ -0,0 +1,30 @@
+// LES BOUCLES
+
+/* La boucle permeter une répétition grâce à une condition */
+/* On utilise la condition while qui veut dire tant que */
+
+let numero = 0;
+
+while(numero < 9) {
+
+
+numero = numero + 3
+
+
+}
+
+/* La boucle FOR est autre boucle qui permet d'exécuter un certain nombre de fois en précisant
+manuellement l'intervalle pour laquelle on souhaite faire la boucle */
+
+for(let compteur = 0 ; compteur < 3; compteur ++){
+// let compteur = 0 déclaration de la variable à 0
+// let compteur > 3 test de la valeur est + petit que 3
+// let compteur++ a chaque tour, j'inclémente +1 à  la valeur de la variable
+
+console.log("Le compteur est : "+ compteur); // affiche le compteur
+
+}
+
+// i = 0; i est l'indice c'est à dire c'est le code de positionnement
+// i < nom.length permet d'afficher le nombre d'élément du tableau 
+// i++ permet d'ajouter +1 à chaque liste/tour
