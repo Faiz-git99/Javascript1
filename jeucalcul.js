d68bbd9cdbd7eec337d1a5140781a9cac9eff941
Faiz-git99 <marifaiz97@gmail.com>
2024-10-20 23:00:19 +0300

    tentative de foction

--- jeucalcul.js
+++ jeucalcul.js
@@ -1,7 +1,10 @@
 // Déclarer une fonction qui va calculer l'âge automatiquement
 
-function calculeAuto(){
-let age = document.querySelector("#auto")
-document.calculeAuto
-    ;
+function calculeAuto(param1,param2){
+let age = param1 - param2;
+   return(age);
 }
+let result = (2024-2000);
+
+let classe= document.querySelector("auto");
+.appendChild(result);
